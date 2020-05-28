aed22942453474a192067ba80eac22e23877edc2
YagoErnandes <yago700@gmail.com>
2020-05-27 23:54:57 -0300

    navegador conectado

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import store from './store'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import Home from './pages/Home'
-import Result from './pages/Result'
-
-import { Page } from './styles'
+import Routes from './routes'
 
 const App: React.FC = () => {
 	return (
 		<Provider store={store}>
-			<Router>
-				<Switch>
-					<Page>
-						<Route path="/">
-							<Result />
-						</Route>
-						<Route path="/home">
-							<Home />
-						</Route>
-					</Page>
-				</Switch>
-			</Router>
+			<Routes />
 		</Provider>
 	)
 }
